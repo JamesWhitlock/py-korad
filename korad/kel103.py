08636688b93402573eb8cb18177d5b92565cae7e
James Whitlock <james.whitlock@gmail.com>
2025-04-07 14:32:03 +0100

    Split out koradUdpComm to enable reuse

--- korad/kel103.py
+++ korad/kel103.py
@@ -1,47 +1,5 @@
 import re
-import socket
-import time
-
-class koradUdpComm(object):
-
-    def __init__(self, localAddress, deviceAddress, port):
-        self.clientAddress = (localAddress, port)
-        self.deviceAddress = (deviceAddress, port)
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    def connect(self):
-        self.sock.bind(self.clientAddress)
-        self.sock.settimeout(1.0)
-
-    def close(self):
-        self.sock.close()
-
-    def udpSendRecv(self, message):
-        # build the message
-        messageb = bytearray()
-        messageb.extend(map(ord, message))
-        messageb.append(0x0a)
-
-        startTime = time.time()
-        while True:
-            sent = self.sock.sendto(messageb, self.deviceAddress)
-            self.sock.settimeout(1.0)
-            data, server = self.sock.recvfrom(1024)
-            if data:
-                return data.decode('utf-8')
-
-            if time.time() - startTime > 3:
-                print ("UDP timeout")
-                return " "
-
-    def udpSend(self, message):
-        # build the message
-        messageb = bytearray()
-        messageb.extend(map(ord, message))
-        messageb.append(0x0a)
-
-        sent = self.sock.sendto(messageb, self.deviceAddress)
-
+from .korad_udp import koradUdpComm
 
 class kel103(object):
     def __init__(self, localAddress, deviceAddress, port):
